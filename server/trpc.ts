7612315e174cc01af2844ef65921b710908dae00
ABDUL BASIT <absh5506@gmail.com>
2026-07-15 04:55:46 -0700

    security: require operator session for protected procedures

--- server/trpc.ts
+++ server/trpc.ts
@@ -1,10 +1,12 @@
-import { initTRPC } from '@trpc/server';
-import { Context } from './context';
+import { initTRPC, TRPCError } from '@trpc/server';
 import superjson from 'superjson';
+import type { Context } from './context';
 
-const t = initTRPC.context<Context>().create({
-  transformer: superjson,
-});
+const t = initTRPC.context<Context>().create({ transformer: superjson });
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
+export const protectedProcedure = t.procedure.use(({ ctx, next }) => {
+  if (!ctx.session) throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Operator session required' });
+  return next({ ctx: { ...ctx, session: ctx.session } });
+});
